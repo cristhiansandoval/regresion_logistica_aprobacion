e83ec7252662e3fef9e87ae238592f009e36d233
LauraIFernandez <86074568+LauraIFernandez@users.noreply.github.com>
2021-08-30 18:50:34 -0300

    Add files via upload

--- regresion_logistica_aprobacion.py
+++ regresion_logistica_aprobacion.py
@@ -4,10 +4,13 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn import model_selection
+from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
 
 
@@ -34,36 +37,76 @@ print ('Descripcion de la matriz X')
 dataframe1=dataframe.drop(['Alumno', 'Resultado'],axis=1)
 print (dataframe1.describe())
 
-#### Se crea y entrena el modelo ####
+#### Se crea y entrenan los modelos de regresion logistica ####
 X = np.array(dataframe.drop(['Alumno', 'Resultado'],1))
 y = np.array(dataframe['Resultado'])
 print ('La dimension de la matriz X es: ', X.shape)
-modelo = linear_model.LogisticRegression()
-modelo.fit(X,y)
-predicciones = modelo.predict(X)
-coeficientes = modelo.coef_
-termino_independiente = modelo.intercept_
-
-# Se visualizan los resultados del entrenamiento
-print('Predicciones: \n', predicciones)
-# Se visualizan los coeficientes obtenidos.
-print('Coeficientes: \n', coeficientes)
-# Se visualizan el valor donde corta el eje Y (en X=0)
-print('Termino independiente: \n', termino_independiente)
-
-
-### Se valida el modelo ####
-validation_size = 0.20
-seed = 7
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
-predicciones = modelo.predict(X_validation)
-# Se visualiza el Error Cuadratico Medio
-print("El error cuadratico medio: %.2f" % mean_squared_error(Y_validation, predicciones))
-# Se visualiza el Puntaje de Varianza. El mejor puntaje es un 1.0
-print('El puntaje de varianza es: %.2f' % r2_score(Y_validation, predicciones))
-# Reporte de resultados #
-print('La matriz de confusion es: \n',confusion_matrix(Y_validation, predicciones))
-print('El reporte de clasificacion es: \n',classification_report(Y_validation, predicciones))
-#### Clasificacion de nuevos registros  ####
-X_new = pd.DataFrame({'Sexo':['1'],'TrabajoPractico1': ['9.5'],'TrabajoPractico2': ['9'],'TrabajoPractico3': ['8'],'TrabajoPractico4': ['8'],'TrabajoPractico5': ['9'],'TrabajoPractico6': ['10'],'TrabajoPractico7': ['10'],'TrabajoPractico8': ['10'],'TrabajoPractico9': ['0'],'TrabajoPractico10': ['0'], 'NotaPrimerParcial':  ['10'], 'NotaSegundoParcial':  ['8.5'],'NotaTrabajoFinal':  ['7']})
-print('La nueva clasificacion es: ', modelo.predict(X_new))
+print('------------------------------------------------------------')
+print('Algoritmo de Regresion Logistica')
+print('------------------------------------------------------------')
+# Se modifica iterativamente el hiperparametro C 
+for i, c in enumerate([1, 0.1, 0.01]):
+    print('------------------------------------------------------------')
+    print('Modelos de diferentes penalizaciones con hiperparametro C = ', c)
+    print('------------------------------------------------------------')
+    # Se definen modelos para penalizaciones l1, l2, elasticnet
+    modelo_n = linear_model.LogisticRegression(C=c, penalty='none')
+    modelo_l2 = linear_model.LogisticRegression(C=c, penalty='l2')
+    modelo_n.fit(X,y)
+    modelo_l2.fit(X,y)
+    predicciones_n = modelo_n.predict(X)
+    predicciones_l2 = modelo_l2.predict(X)
+    coeficientes_n = modelo_n.coef_
+    coeficientes_l2 = modelo_l2.coef_
+    termino_independiente_n = modelo_n.intercept_
+    termino_independiente_l2 = modelo_l2.intercept_
+    # Se visualizan los resultados del entrenamiento
+    print('Predicciones para No penalizado: \n', predicciones_n)
+    print('Predicciones para L2: \n', predicciones_l2)
+    # Se visualizan los coeficientes obtenidos.
+    print('Coeficientes para No penalizado: \n', coeficientes_n)
+    print('Coeficientes para L2: \n', coeficientes_l2)
+    # Se visualizan los valores donde cortan al eje Y (en X=0)
+    print('Termino independiente para No penalizado: \n', termino_independiente_n)
+    print('Termino independiente para L2: \n', termino_independiente_l2)
+    ### Se valida el modelo ####
+    validation_size = 0.20
+    seed = 7
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+    predicciones_n = modelo_n.predict(X_validation)
+    predicciones_l2 = modelo_l2.predict(X_validation)
+    # Se visualiza el Error Cuadratico Medio
+    print("El error cuadratico medio para No penaizado: %.2f" % mean_squared_error(Y_validation, predicciones_n))
+    print("El error cuadratico medio para L2: %.2f" % mean_squared_error(Y_validation, predicciones_l2))
+    # Se visualiza el Puntaje de Varianza. El mejor puntaje es un 1.0
+    print('Puntaje de varianza para No penalizado: %.2f' % r2_score(Y_validation, predicciones_n))
+    print('Puntaje de varianza para L2: %.2f' % r2_score(Y_validation, predicciones_l2))
+    # Reporte de resultados #
+    print('El reporte de clasificacion para No penalizado es: \n',classification_report(Y_validation, predicciones_n))
+    print('El reporte de clasificacion para L2 es: \n',classification_report(Y_validation, predicciones_l2))
+    #### Clasificacion de nuevos registros  ####
+    X_new = pd.DataFrame({'Sexo':['1'],'TrabajoPractico1': ['9.5'],'TrabajoPractico2': ['9'],'TrabajoPractico3': ['8'],'TrabajoPractico4': ['8'],'TrabajoPractico5': ['9'],'TrabajoPractico6': ['10'],'TrabajoPractico7': ['10'],'TrabajoPractico8': ['10'],'TrabajoPractico9': ['0'],'TrabajoPractico10': ['0'], 'NotaPrimerParcial':  ['10'], 'NotaSegundoParcial':  ['8.5'],'NotaTrabajoFinal':  ['7']})
+    print('La nueva clasificacion para No penalizado es: ', modelo_n.predict(X_new))
+    print('La nueva clasificacion para L2 es: ', modelo_l2.predict(X_new))
+    print(' ')
+#### Se crea y entrenan los modelos de SVM #### 
+print('------------------------------------------------------------')
+print('Algoritmo SVM generando matrices de confusion')
+print('------------------------------------------------------------')
+# Se modifica iterativamente el hiperparametro C 
+for i, c in enumerate([1, 0.1, 0.01]):
+    print('------------------------------------------------------------')
+    print('Modelos de diferentes penalizaciones con hiperparametro C = ', c)
+    print('------------------------------------------------------------')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    # Ejecutando el clasificador con distintos valores de C
+    classifier = svm.SVC(kernel='linear', C=c).fit(X_train, y_train)
+    np.set_printoptions(precision=2)
+    # Se grafican las matrices de cifusion
+    titles_options = [("Matriz de confusion, sin normalizacion", None), ("Matriz de confusion normalizada", 'true')]
+    for title, normalize in titles_options:
+        disp = plot_confusion_matrix(classifier, X_test, y_test, cmap=plt.cm.Greens, normalize=normalize)
+        disp.ax_.set_title(title)
+        print(title)
+        print(disp.confusion_matrix)
+    plt.show()
