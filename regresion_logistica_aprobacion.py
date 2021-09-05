f8244896d9bbdb62071abbf10fd3cfb99bc46df0
LauraIFernandez <86074568+LauraIFernandez@users.noreply.github.com>
2021-09-05 13:03:14 -0300

    Modelo Arboles de Decisión y Validaciones
    Modelo de Arboles de Decisión y Validaciones de Modelos

--- regresion_logistica_aprobacion.py
+++ regresion_logistica_aprobacion.py
@@ -4,10 +4,15 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn import model_selection
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
 
 
@@ -38,6 +43,9 @@ print (dataframe1.describe())
 X = np.array(dataframe.drop(['Alumno', 'Resultado'],1))
 y = np.array(dataframe['Resultado'])
 print ('La dimension de la matriz X es: ', X.shape)
+print('------------------------------------------------------------')
+print('Algoritmo de Regresion Logistica')
+print('------------------------------------------------------------')
 # Se modifica iterativamente el hiperparametro C 
 for i, c in enumerate([1, 0.1, 0.01]):
     print('------------------------------------------------------------')
@@ -63,7 +71,7 @@ for i, c in enumerate([1, 0.1, 0.01]):
     # Se visualizan los valores donde cortan al eje Y (en X=0)
     print('Termino independiente para No penalizado: \n', termino_independiente_n)
     print('Termino independiente para L2: \n', termino_independiente_l2)
-    ### Se valida el modelo ####
+    ### Se valida la generalizacion del modelo con particion de muestra por la claridad de resultados que aporta####
     validation_size = 0.20
     seed = 7
     X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
@@ -76,12 +84,126 @@ for i, c in enumerate([1, 0.1, 0.01]):
     print('Puntaje de varianza para No penalizado: %.2f' % r2_score(Y_validation, predicciones_n))
     print('Puntaje de varianza para L2: %.2f' % r2_score(Y_validation, predicciones_l2))
     # Reporte de resultados #
-    print('La matriz de confusion para No penalizado es: \n',confusion_matrix(Y_validation, predicciones_n))
     print('El reporte de clasificacion para No penalizado es: \n',classification_report(Y_validation, predicciones_n))
-    print('La matriz de confusion para L2 es: \n',confusion_matrix(Y_validation, predicciones_l2))
     print('El reporte de clasificacion para L2 es: \n',classification_report(Y_validation, predicciones_l2))
     #### Clasificacion de nuevos registros  ####
     X_new = pd.DataFrame({'Sexo':['1'],'TrabajoPractico1': ['9.5'],'TrabajoPractico2': ['9'],'TrabajoPractico3': ['8'],'TrabajoPractico4': ['8'],'TrabajoPractico5': ['9'],'TrabajoPractico6': ['10'],'TrabajoPractico7': ['10'],'TrabajoPractico8': ['10'],'TrabajoPractico9': ['0'],'TrabajoPractico10': ['0'], 'NotaPrimerParcial':  ['10'], 'NotaSegundoParcial':  ['8.5'],'NotaTrabajoFinal':  ['7']})
     print('La nueva clasificacion para No penalizado es: ', modelo_n.predict(X_new))
     print('La nueva clasificacion para L2 es: ', modelo_l2.predict(X_new))
     print(' ')
+#### Se crea y entrenan los modelos de SVM #### 
+print('------------------------------------------------------------')
+print('Algoritmo SVM generando matrices de confusion')
+print('------------------------------------------------------------')
+# Se modifica iterativamente el hiperparametro C 
+for i, c in enumerate([1, 0.1, 0.01]):
+    print('------------------------------------------------------------')
+    print('Modelos de diferentes penalizaciones con hiperparametro C = ', c)
+    print('------------------------------------------------------------')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    # Ejecutando el clasificador con distintos valores de C
+    classifier = svm.SVC(kernel='linear', C=c).fit(X_train, y_train)
+    np.set_printoptions(precision=2)
+    # Se grafican las matrices de cifusion
+    titles_options = [("Matriz de confusion, sin normalizacion", None), ("Matriz de confusion normalizada", 'true')]
+    for title, normalize in titles_options:
+        disp = plot_confusion_matrix(classifier, X_test, y_test, cmap=plt.cm.Greens, normalize=normalize)
+        disp.ax_.set_title(title)
+        print(title)
+        print(disp.confusion_matrix)
+    plt.show()
+    ### Se valida la generalizacion del modelo con particion de muestra por la claridad de resultados que aporta####
+    validation_size = 0.20
+    seed = 7
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+    predicciones = classifier.predict(X_validation)
+    # Se visualiza el Error Cuadratico Medio
+    print("El error cuadratico medio es: %.2f" % mean_squared_error(Y_validation, predicciones))
+    # Se visualiza el Puntaje de Varianza. El mejor puntaje es un 1.0
+    print('Puntaje de varianza es: %.2f' % r2_score(Y_validation, predicciones))
+    # Reporte de resultados #
+    print('El reporte de clasificacion es: \n',classification_report(Y_validation, predicciones))
+#### Se crea y entrenan los modelos de Arboles de Decision #### 
+print('------------------------------------------------------------')
+print('Algoritmo de arboles de decision')
+print('------------------------------------------------------------')
+# Estructura del arbol
+# 
+# El clasificador de decision tiene un atributo llamado tree_ que permite el acceso a atributos
+# de bajo nivel, como node_countel numero total de nodos y max_depth la profundidad máxima del arbol. 
+# También almacena toda la estructura del arbol binario, representada como una serie de matrices 
+# paralelas. El elemento i-ésimo de cada matriz contiene información sobre el nodo i. 
+# El nodo 0 es la raíz del árbol. Algunas de las matrices solo se aplican a hojas o nodos divididos. 
+# En este caso, los valores de los nodos del otro tipo son arbitrarios. Por ejemplo, las matrices 
+# feature y threshold solo se aplican a los nodos divididos. Por tanto, los valores de los nodos 
+# hoja en estas matrices son arbitrarios.
+#
+# Entre estos array, tenemos:
+#
+#        children_left[i]: id del hijo izquierdo del nodo io -1 si el nodo hoja
+#        children_right[i]: id del hijo derecho del nodo io -1 si el nodo hoja
+#        feature[i]: característica utilizada para dividir el nodo i
+#        threshold[i]: valor umbral en el nodo i
+#        n_node_samples[i]: el número de muestras de entrenamiento que llegan al nodo i
+#        impurity[i]: la impureza en el nodo i
+#
+# Usando las matrices, podemos atravesar la estructura del árbol para calcular varias propiedades. 
+# A continuacion, calcularemos la profundidad de cada nodo y si es una hoja o no.
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+for i, maximo_hojas in enumerate([2, 3, 4]):
+    print('------------------------------------------------------------')
+    print('Modelos de arbol con hiperparametro numero maximo de hojas = ', maximo_hojas)
+    print('------------------------------------------------------------')
+    clf = DecisionTreeClassifier(max_leaf_nodes=maximo_hojas, random_state=0)
+    clf.fit(X_train, y_train)
+    n_nodes = clf.tree_.node_count
+    children_left = clf.tree_.children_left
+    children_right = clf.tree_.children_right
+    feature = clf.tree_.feature
+    threshold = clf.tree_.threshold
+    node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+    is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+    stack = [(0, 0)]  # Se comienza con la identificación del nodo raíz (0) y su profundidad (0) 
+    while len(stack) > 0:
+        # `pop` asegura que cada nodo solo se visite una vez 
+        node_id, depth = stack.pop()
+        node_depth[node_id] = depth
+        # Si el hijo izquierdo y derecho de un nodo no es el mismo, tenemos un nodo dividido 
+        is_split_node = children_left[node_id] != children_right[node_id]
+        # Si es un nodo dividido, agregue los elementos secundarios izquierdo y derecho y 
+        # la profundidad a la "pila" para que podamos recorrerlos.
+        if is_split_node:
+            stack.append((children_left[node_id], depth + 1))
+            stack.append((children_right[node_id], depth + 1))
+        else:
+            is_leaves[node_id] = True
+    print("La estructura de arbol binaria tiene {n} nodos y tiene la siguiente estructura de arbol:\n".format(n=n_nodes))
+    for i in range(n_nodes):
+        if is_leaves[i]:
+            print("{space}nodo={node} es nodo hoja.".format( space=node_depth[i] * "\t", node=i))
+        else:
+            print("{space}nodo={node} is un nodo divido: va al nodo {left} si X[:, {feature}] <= {threshold} "
+                  "sino va al nodo {right}.".format(
+                    space=node_depth[i] * "\t",
+                    node=i,
+                    left=children_left[i],
+                    feature=feature[i],
+                    threshold=threshold[i],
+                    right=children_right[i]))
+     # Se grafican los resultados a modo de comparacion.
+     ### Se valida la generalizacion del modelo con particion de muestra por la claridad de resultados que aporta####
+    validation_size = 0.20
+    seed = 7
+    X_trains, X_validation, Y_trains, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+    predicciones = clf.predict(X_validation)
+    # Se visualiza el Error Cuadratico Medio
+    print("El error cuadratico medio es: %.2f" % mean_squared_error(Y_validation, predicciones))
+    # Se visualiza el Puntaje de Varianza. El mejor puntaje es un 1.0
+    print('Puntaje de varianza es: %.2f' % r2_score(Y_validation, predicciones))
+    # Reporte de resultados #
+    print('El reporte de clasificacion es: \n',classification_report(Y_validation, predicciones))
+    tree.plot_tree(clf)
+    plt.show()
+
