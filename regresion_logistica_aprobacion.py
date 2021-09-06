58b8a2797b8eed742a384fc6cebfa3d20b78f836
cristhiansandoval <69112639+cristhiansandoval@users.noreply.github.com>
2021-09-06 16:54:59 -0300

    Eliminación de la variable TrabajoPractico9
    Ejecución de los modelos eliminando la variable Trabajo Practico 9

--- regresion_logistica_aprobacion.py
+++ regresion_logistica_aprobacion.py
@@ -826,3 +826,158 @@ for i, maximo_hojas in enumerate([2, 3, 4]):
     print('El reporte de clasificacion es: \n',classification_report(Y_validation, predicciones))
     tree.plot_tree(clf)
     plt.show()
+
+print ('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+print ('+ Eliminación de la variable TrabajoPractico9 para la prediccion de etiquetas ')
+print ('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+print ('')
+##### Se describe la matriz de datos sin las variables alumno, nota de TP9, TP10, 1er, 2do parcial, trabajo final y resultado  ####
+print ('Descripcion de la matriz X')
+dataframe1=dataframe.drop(['Alumno', 'NotaTrabajoFinal', 'TrabajoPractico9', 'TrabajoPractico10', 'NotaPrimerParcial', 'NotaSegundoParcial', 'Resultado'],axis=1)
+print (dataframe1.describe())
+
+#### Se crea y entrenan los modelos de regresion logistica ####
+X = np.array(dataframe.drop(['Alumno', 'NotaTrabajoFinal', 'TrabajoPractico9', 'TrabajoPractico10', 'NotaPrimerParcial', 'NotaSegundoParcial', 'Resultado'],1))
+y = np.array(dataframe['Resultado'])
+print ('La dimension de la matriz X es: ', X.shape)
+print('------------------------------------------------------------')
+print('Algoritmo de Regresion Logistica')
+print('------------------------------------------------------------')
+# Se modifica iterativamente el hiperparametro C 
+for i, c in enumerate([1, 0.1, 0.01]):
+    print('------------------------------------------------------------')
+    print('Modelos de diferentes penalizaciones con hiperparametro C = ', c)
+    print('------------------------------------------------------------')
+    # Se definen modelos para penalizaciones l1, l2, elasticnet
+    modelo_n = linear_model.LogisticRegression(C=c, penalty='none')
+    modelo_l2 = linear_model.LogisticRegression(C=c, penalty='l2')
+    modelo_n.fit(X,y)
+    modelo_l2.fit(X,y)
+    predicciones_n = modelo_n.predict(X)
+    predicciones_l2 = modelo_l2.predict(X)
+    coeficientes_n = modelo_n.coef_
+    coeficientes_l2 = modelo_l2.coef_
+    termino_independiente_n = modelo_n.intercept_
+    termino_independiente_l2 = modelo_l2.intercept_
+    # Se visualizan los resultados del entrenamiento
+    print('Predicciones para No penalizado: \n', predicciones_n)
+    print('Predicciones para L2: \n', predicciones_l2)
+    # Se visualizan los coeficientes obtenidos.
+    print('Coeficientes para No penalizado: \n', coeficientes_n)
+    print('Coeficientes para L2: \n', coeficientes_l2)
+    # Se visualizan los valores donde cortan al eje Y (en X=0)
+    print('Termino independiente para No penalizado: \n', termino_independiente_n)
+    print('Termino independiente para L2: \n', termino_independiente_l2)
+    ### Se valida la generalizacion del modelo con particion de muestra por la claridad de resultados que aporta####
+    validation_size = 0.20
+    seed = 7
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+    predicciones_n = modelo_n.predict(X_validation)
+    predicciones_l2 = modelo_l2.predict(X_validation)
+    # Se visualiza el Error Cuadratico Medio
+    print("El error cuadratico medio para No penaizado: %.2f" % mean_squared_error(Y_validation, predicciones_n))
+    print("El error cuadratico medio para L2: %.2f" % mean_squared_error(Y_validation, predicciones_l2))
+    # Se visualiza el Puntaje de Varianza. El mejor puntaje es un 1.0
+    print('Puntaje de varianza para No penalizado: %.2f' % r2_score(Y_validation, predicciones_n))
+    print('Puntaje de varianza para L2: %.2f' % r2_score(Y_validation, predicciones_l2))
+    # Reporte de resultados #
+    print('El reporte de clasificacion para No penalizado es: \n',classification_report(Y_validation, predicciones_n))
+    print('El reporte de clasificacion para L2 es: \n',classification_report(Y_validation, predicciones_l2))
+    #### Clasificacion de nuevos registros  ####
+    X_new = pd.DataFrame({'Sexo':['1'],'TrabajoPractico1': ['9.5'],'TrabajoPractico2': ['9'],'TrabajoPractico3': ['8'],'TrabajoPractico4': ['8'],'TrabajoPractico5': ['9'],'TrabajoPractico6': ['10'],'TrabajoPractico7': ['10'],'TrabajoPractico8': ['10']})
+    print('La nueva clasificacion para No penalizado es: ', modelo_n.predict(X_new))
+    print('La nueva clasificacion para L2 es: ', modelo_l2.predict(X_new))
+    print(' ')
+#### Se crea y entrenan los modelos de SVM #### 
+print('------------------------------------------------------------')
+print('Algoritmo SVM generando matrices de confusion')
+print('------------------------------------------------------------')
+# Se modifica iterativamente el hiperparametro C 
+for i, c in enumerate([1, 0.1, 0.01]):
+    print('------------------------------------------------------------')
+    print('Modelos de diferentes penalizaciones con hiperparametro C = ', c)
+    print('------------------------------------------------------------')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    # Ejecutando el clasificador con distintos valores de C
+    classifier = svm.SVC(kernel='linear', C=c).fit(X_train, y_train)
+    np.set_printoptions(precision=2)
+    # Se grafican las matrices de cifusion
+    titles_options = [("Matriz de confusion, sin normalizacion", None), ("Matriz de confusion normalizada", 'true')]
+    for title, normalize in titles_options:
+        disp = plot_confusion_matrix(classifier, X_test, y_test, cmap=plt.cm.Greens, normalize=normalize)
+        disp.ax_.set_title(title)
+        print(title)
+        print(disp.confusion_matrix)
+    plt.show()
+    ### Se valida la generalizacion del modelo con particion de muestra por la claridad de resultados que aporta####
+    validation_size = 0.20
+    seed = 7
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+    predicciones = classifier.predict(X_validation)
+    # Se visualiza el Error Cuadratico Medio
+    print("El error cuadratico medio es: %.2f" % mean_squared_error(Y_validation, predicciones))
+    # Se visualiza el Puntaje de Varianza. El mejor puntaje es un 1.0
+    print('Puntaje de varianza es: %.2f' % r2_score(Y_validation, predicciones))
+    # Reporte de resultados #
+    print('El reporte de clasificacion es: \n',classification_report(Y_validation, predicciones))
+#### Se crea y entrenan los modelos de Arboles de Decision #### 
+print('------------------------------------------------------------')
+print('Algoritmo de arboles de decision')
+print('------------------------------------------------------------')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+for i, maximo_hojas in enumerate([2, 3, 4]):
+    print('------------------------------------------------------------')
+    print('Modelos de arbol con hiperparametro numero maximo de hojas = ', maximo_hojas)
+    print('------------------------------------------------------------')
+    clf = DecisionTreeClassifier(max_leaf_nodes=maximo_hojas, random_state=0)
+    clf.fit(X_train, y_train)
+    n_nodes = clf.tree_.node_count
+    children_left = clf.tree_.children_left
+    children_right = clf.tree_.children_right
+    feature = clf.tree_.feature
+    threshold = clf.tree_.threshold
+    node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+    is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+    stack = [(0, 0)]  # Se comienza con la identificación del nodo raíz (0) y su profundidad (0) 
+    while len(stack) > 0:
+        # `pop` asegura que cada nodo solo se visite una vez 
+        node_id, depth = stack.pop()
+        node_depth[node_id] = depth
+        # Si el hijo izquierdo y derecho de un nodo no es el mismo, tenemos un nodo dividido 
+        is_split_node = children_left[node_id] != children_right[node_id]
+        # Si es un nodo dividido, agregue los elementos secundarios izquierdo y derecho y 
+        # la profundidad a la "pila" para que podamos recorrerlos.
+        if is_split_node:
+            stack.append((children_left[node_id], depth + 1))
+            stack.append((children_right[node_id], depth + 1))
+        else:
+            is_leaves[node_id] = True
+    print("La estructura de arbol binaria tiene {n} nodos y tiene la siguiente estructura de arbol:\n".format(n=n_nodes))
+    for i in range(n_nodes):
+        if is_leaves[i]:
+            print("{space}nodo={node} es nodo hoja.".format( space=node_depth[i] * "\t", node=i))
+        else:
+            print("{space}nodo={node} is un nodo divido: va al nodo {left} si X[:, {feature}] <= {threshold} "
+                  "sino va al nodo {right}.".format(
+                    space=node_depth[i] * "\t",
+                    node=i,
+                    left=children_left[i],
+                    feature=feature[i],
+                    threshold=threshold[i],
+                    right=children_right[i]))
+     # Se grafican los resultados a modo de comparacion.
+     ### Se valida la generalizacion del modelo con particion de muestra por la claridad de resultados que aporta####
+    validation_size = 0.20
+    seed = 7
+    X_trains, X_validation, Y_trains, Y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+    predicciones = clf.predict(X_validation)
+    # Se visualiza el Error Cuadratico Medio
+    print("El error cuadratico medio es: %.2f" % mean_squared_error(Y_validation, predicciones))
+    # Se visualiza el Puntaje de Varianza. El mejor puntaje es un 1.0
+    print('Puntaje de varianza es: %.2f' % r2_score(Y_validation, predicciones))
+    # Reporte de resultados #
+    print('El reporte de clasificacion es: \n',classification_report(Y_validation, predicciones))
+    tree.plot_tree(clf)
+    plt.show()
